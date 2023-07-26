f6d0e8c5ee6d2f49dd13d86c6105d9dff6ea341b
Iasmim Bitencourt <iasmim.bit@gmail.com>
2023-07-26 19:00:46 -0300

    feat: add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+export default {
+    COLORS: {
+        WHITE: '#FFFFFF',
+
+        RED_300: '#BF3B44',
+        RED_200: '#F3BABD',
+        RED_100: '#F4E6E7',
+
+        GREEN_300: '#639339',
+        GREEN_200: '#CBE4B4',
+        GREEN_100: '#E5F0DB',
+
+        GRAY_700: '#1B1D1E',
+        GRAY_600: '#333638',
+        GRAY_500: '#5C6265',
+        GRAY_400: '#B9BBBC',
+        GRAY_300: '#DDDEDF',
+        GRAY_200: '#EFF0F0',
+        GRAY_100: '#FAFAFA',
+
+    },
+    FONT_FAMILY: {
+      REGULAR: 'NunitoSans_400Regular',
+      BOLD: 'NunitoSans_700Bold'
+    },
+    FONT_SIZE: {
+        XSM: 12,
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24,
+        XXL: 32
+    }
+};
